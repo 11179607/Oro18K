74aea7cac8ccedc626b943cedb2c4992cfd9b171
11179607 <marloncdela@gmail.com>
2025-07-02 18:49:26 -0500

    cambiomas

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ function sellProduct() {
       </head>
       <body>
         <div class="factura-box">
-          <h2>💎 Factura de Venta - Destello de Oro 18K</h2>
+          <h2>💎Factura de Venta-Destello de Oro 18K💎</h2>
           <p><span class="label">Cliente:</span> ${client}</p>
           <p><span class="label">Producto:</span> ${product}</p>
           <p><span class="label">Cantidad:</span> ${qty}</p>
